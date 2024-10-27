14f97809d0fb26415cc66d81bb1b07e08a198c3f
Giacomo-Orsucci <orsucci.giacomo@gmail.com>
2024-10-27 21:06:25 +0100

    It's finally working

---
+++ accuracy.py
@@ -0,0 +1,135 @@
+from models import StegaStampDecoder
+import torch
+
+import torchvision.transforms as transforms
+import torchvision
+import numpy as np
+
+import torch
+from torchvision.utils import save_image
+import os
+import argparse
+import cv2
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
+import glob
+import PIL
+
+parser = argparse.ArgumentParser()
+
+transform = transforms.Compose([
+    transforms.ToTensor()  # Convert the image to a tensor
+])
+
+
+#program to evaluate the accuracy on a set of images already generated
+#experimentally the accuracy that we have in this way is slightly different (+-0.005) on the total
+#if we compare it with the accuracy obtained from a set of images generated and directly analized without saving them before
+
+
+
+class CustomImageFolder():
+    def __init__(self, data_dir, transform=None):
+        self.data_dir = data_dir #path with the dataset for the training specified via CLI
+        self.filenames = glob.glob(os.path.join(data_dir, "*.png")) #to get all the png image's paths 
+        self.filenames.extend(glob.glob(os.path.join(data_dir, "*.jpeg"))) #to add all the jpeg images' path 
+        self.filenames.extend(glob.glob(os.path.join(data_dir, "*.jpg"))) #to add all the jpg images' path 
+        self.filenames = sorted(self.filenames) #order the file name in ascendent order
+        self.transform = transform
+
+    #return the image at the specified index
+    def __getitem__(self, idx):
+        filename = self.filenames[idx]
+        image = PIL.Image.open(filename)
+        if self.transform:
+            image = self.transform(image)
+        return image, 0
+
+    def __len__(self):
+        return len(self.filenames)
+
+
+def main(image_directory, dec_path_pre):
+
+    #Set the device
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+
+    fingerprint = torch.tensor([0,1,0,0,0,1,0,0,0,1,0,0,0,0,1,0,1,1,1,0,1,0,1,1,1,1,1,1,1,1,0,0,1,1,1,
+                                0,1,0,0,0,0,0,1,1,1,1,1,0,1,1,0,1,0,1,0,1,1,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,
+                                0,1,0,1,1,1,0,1,0,1,0,1,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,0]).to(device) #embedded fingerprint
+                                
+
+
+    args = parser.parse_args()
+
+    IMAGE_RESOLUTION = 128
+    IMAGE_CHANNELS = 1
+
+
+    FINGERPRINT_SIZE = len(fingerprint)
+
+
+
+    RevealNet_pre = StegaStampDecoder( #decoder and parameters passing
+            IMAGE_RESOLUTION, IMAGE_CHANNELS, fingerprint_size=FINGERPRINT_SIZE
+        )
+    RevealNet_pre.load_state_dict(torch.load(dec_path_pre))
+    RevealNet_pre = RevealNet_pre.to(device)
+    RevealNet_pre.eval()
+
+
+    dataset = CustomImageFolder(image_directory, transform=transform)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=0)
+    bitwise_accuracy = 0;
+
+    j=0
+    for images, _ in tqdm(dataloader):
+        #print(images.shape)
+        y_channel_list = []
+
+        for image in images:
+            image = image.permute(1, 2, 0).cpu().numpy()
+            print("Singola immagine")
+            print(image.shape)
+            #image = (image * 255).astype(np.uint8)
+
+            # Converti l'immagine RGB in YUV usando OpenCV
+            image_yuv = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+            #print(image_yuv)
+            y_channel, u_channel, v_channel = cv2.split(image)
+            image = y_channel
+            image = torch.from_numpy(image).unsqueeze(0)
+            print("shape di image")
+            print(image.shape)
+
+            
+            y_channel_list.append(image)
+        
+        images_y_batch = torch.stack(y_channel_list).to(device)
+
+
+        print("shape di batch")
+        print(images_y_batch.shape)
+
+
+        detected_fingerprints = RevealNet_pre(images_y_batch)
+        detected_fingerprints = (detected_fingerprints > 0).long()
+
+        print("detected fingerprint")
+        print(detected_fingerprints.shape)
+        print(detected_fingerprints[0])
+                
+        
+        for i in enumerate(detected_fingerprints):
+            j = j + 1
+            #to calculate the accuracy in retrieving the fingerprint (eventually perturbated)
+            bitwise_accuracy += (detected_fingerprints[i].detach() == fingerprint).float().mean().sum().item()
+            #print(bitwise_accuracy)
+
+    
+        
+    print(j)
+    bitwise_accuracy = bitwise_accuracy/j
+    print(bitwise_accuracy)
+    return bitwise_accuracy
--- gaussian.py
+++ gaussian.py
@@ -7,54 +7,10 @@ import torch
 from models import StegaStampDecoder
 import matplotlib.pyplot as plt
 from graphs import plotting
-from psnr import main
-import torchvision.transforms as transforms
-import glob
-from torch.utils.data import Dataset, DataLoader
-from tqdm import tqdm
-from torchvision.transforms import ToPILImage
+from psnr import main as mainp
+from accuracy import main as maina
 
 
-
-
-transform = transforms.Compose([
-    transforms.ToTensor()  # Convert the image to a tensor
-])
-
-class CustomImageFolder():
-    def __init__(self, data_dir, transform=None):
-        self.data_dir = data_dir #path with the dataset for the training specified via CLI
-        self.filenames = glob.glob(os.path.join(data_dir, "*.png")) #to get all the png image's paths 
-        self.filenames.extend(glob.glob(os.path.join(data_dir, "*.jpeg"))) #to add all the jpeg images' path 
-        self.filenames.extend(glob.glob(os.path.join(data_dir, "*.jpg"))) #to add all the jpg images' path 
-        self.filenames = sorted(self.filenames) #order the file name in ascendent order
-        self.transform = transform
-
-    #return the image at the specified index
-    def __getitem__(self, idx):
-        filename = self.filenames[idx]
-        image = PIL.Image.open(filename)
-        if self.transform:
-            image = self.transform(image)
-        return image, 0
-
-    def __len__(self):
-        return len(self.filenames)
-
-
-def rgb_to_yuv(image_rgb):
-    # Assicurati che l'immagine sia in formato float
-    image_rgb = image_rgb.astype(np.float32)
-
-    # Crea un array per l'immagine YUV
-    yuv_image = np.zeros_like(image_rgb)
-
-    # Applica la conversione
-    yuv_image[..., 0] = 0.299 * image_rgb[..., 0] + 0.587 * image_rgb[..., 1] + 0.114 * image_rgb[..., 2]  # Y
-    yuv_image[..., 1] = -0.14713 * image_rgb[..., 0] - 0.28886 * image_rgb[..., 1] + 0.436 * image_rgb[..., 2]  # U
-    yuv_image[..., 2] = 0.615 * image_rgb[..., 0] - 0.51499 * image_rgb[..., 1] - 0.10001 * image_rgb[..., 2]  # V
-
-    return yuv_image
 
 mean = 0
 std = 0
@@ -66,17 +22,16 @@ psnr_array = []
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 #decoder_path = "/home/giacomo/Desktop/enc_dec_pretrained_celeba/dec.pth"
-#decoder_path = "/media/giacomo/volume/old/trained_byme/dec.pth"
 decoder_path = "/media/giacomo/volume/test_yuv/primo/checkpoints/dec.pth"
 
 #fingerprint embedded in the images
 fingerprint = torch.tensor([0,1,0,0,0,1,0,0,0,1,0,0,0,0,1,0,1,1,1,0,1,0,1,1,1,1,1,1,1,1,0,0,1,1,1,
                             0,1,0,0,0,0,0,1,1,1,1,1,0,1,1,0,1,0,1,0,1,1,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,
-                            0,1,0,1,1,1,0,1,0,1,0,1,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,0]).to(device)
+                            0,1,0,1,1,1,0,1,0,1,0,1,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,0])
 
 #image_directory = '/media/giacomo/hdd_ubuntu/stylegan2_gen_50k'
-#image_directory = '/media/giacomo/volume/old/stylegan2_gen_50k_config-e_25'
 image_directory = '/media/giacomo/volume/test_yuv/stylegan2_gen_50k_config-e_25'
+
 
 
 IMAGE_RESOLUTION = 128
@@ -87,121 +42,95 @@ RevealNet = StegaStampDecoder( #decoder and parameter passing
     IMAGE_RESOLUTION, IMAGE_CHANNELS, fingerprint_size=FINGERPRINT_SIZE
 )
 
-
-RevealNet.load_state_dict(torch.load(decoder_path))
-RevealNet = RevealNet.to(device)  # Move the model to the device
+state_dict = torch.load(decoder_path, map_location=device)
+RevealNet.load_state_dict(state_dict)
+RevealNet.to(device)  # Move the model to the device
 RevealNet.eval()      # Set the model to evaluation mode
 
+bitwise_accuracy = 0
+fingerprint = (fingerprint > 0).long().to(device)
 
-
-dataset = CustomImageFolder(image_directory, transform=transform)
-dataloader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=0)
-
-to_pil = ToPILImage()
 
 for i in range(11):
     #if i ==1:break;
-    bitwise_accuracy = 0
-    
     j=0
-    total=0
-    
-    
-    #m=0
-    
-    
-    images_y_batch = []
-    #for filename in os.listdir(image_directory):
-    for images, _ in tqdm(dataloader):
-        
-        if total==11: break;
-
+    for filename in os.listdir(image_directory):
         y_channel_list = []
 
-        for image in images:
-            #print(images.shape)
-            if total==11: break;
+        j += 1 #to count the number of images in the folder
+
+        print(j)
+        #if j == 10: break
+        
+        if filename.endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff')):
             
-            print(image.shape)
+            img_path = os.path.join(image_directory, filename)
+            img = cv2.imread(img_path,3)
+            
+            #img = img/255 #if we want the images in greyscale
 
-            image = image.permute(1, 2, 0).cpu().numpy()
-            #print(image.shape)
-           #noise = torch.normal(mean=mean, std=std, size=image.shape)
-
-            x, y, channels = image.shape  # Include the third dimension for color channels
+            x, y, channels = img.shape  # Include the third dimension for color channels
 
             # Generate noise with the same shape as that of the image
-            noise = np.random.normal(loc=mean, scale=std, size=(x, y, channels))
-          
-           # print(noise)
+            noise = np.random.normal(loc=mean, scale=std, size=(x, y, channels))  # Adjust noise shape
+
+            # Add the noise to the image
+            img_noised = img + noise
+
+            # Clip the pixel values to be between 0 and 255 and convert to uint8
+            img_noised = np.clip(img_noised, 0, 255).astype(np.uint8)
+
+            # Convert BGR to RGB
+            img_noised_rgb = cv2.cvtColor(img_noised, cv2.COLOR_BGR2RGB)
+             # Convert BGR to YUV
+            img_noised_yuv = cv2.cvtColor(img_noised, cv2.COLOR_BGR2YUV)
+            y_channel, u_channel, v_channel = cv2.split(img_noised_yuv)
+            img_noised_yuv = y_channel
+
+            print("img-noise-yuv")
+            print(img_noised_yuv.shape)
+
+            img_noised_rgb_array = np.array(img_noised_rgb)
+            image_noised_rgb_tensor = torch.from_numpy(img_noised_rgb_array).permute(2, 0, 1).float().to(device)
+
             
-            image_noised = image + noise
-            
-            #print(image_noised.shape)
+            image_noised_yuv_tensor = torch.from_numpy(img_noised_yuv).float().unsqueeze(0)
+            print("dimensione tensore per firma")
+            print(image_noised_yuv_tensor.shape)
+            y_channel_list = []
+            y_channel_list.append(image_noised_yuv_tensor)
+
            
+            images_y_batch = torch.stack(y_channel_list).to(device)
+
+            print("batch shape")
+            print(images_y_batch.shape)
+
+            detected_fingerprints = RevealNet(images_y_batch)
+            detected_fingerprints = (detected_fingerprints > 0).long()
+
+            print("Fingerprint_shape")
+            print(detected_fingerprints.shape)
+        
+            print(detected_fingerprints)
+            #bitwise_accuracy += (detected_fingerprints == fingerprint).float().mean(dim=1).sum().item()
+            #bitwise_accuracy += (detected_fingerprints.detach() == fingerprint).float().mean().sum().item()
+
+            
 
             img_noise_path = os.path.join("/media/giacomo/volume/test_yuv/robustness/gau_noise_std_0-100_style2_25_50k", f"{std}") 
             os.makedirs(img_noise_path, exist_ok=True)
-            png_filename = os.path.join(img_noise_path,f"image{j}.png" )
-            
-            # Convert back to tensor, permute to (C, H, W), and save
-            image_noised_tensor = torch.from_numpy(image_noised).permute(2, 0, 1)  # Convert back to tensor and permute to (C, H, W)
-           # print(image_noised_tensor.shape)
-            #image_noised_tensor = image_noised_tensor.float().to(device)  # Move to device if needed
-
-            to_pil(image_noised_tensor).save(png_filename)
-
-             # Clip the pixel values to be between 0 and 255 and convert to uint8
-            #image_noised = image_noised*255
-            #image_noised = np.clip(image_noised, 0, 255).astype(np.float)
+            png_filename = os.path.join(img_noise_path, filename)
+            PIL.Image.fromarray(img_noised_rgb, "RGB").save(png_filename)
            
-
-            # Converti l'immagine RGB in YUV usando OpenCV
-            image_noised_yuv = rgb_to_yuv(image_noised)
-            #image_noised = image_noised/255
-            y_channel, u_channel, v_channel = cv2.split(image_noised)
-            #print("Valore y")
-            #print(y_channel)
-            y_channel = torch.from_numpy(y_channel).float().unsqueeze(0).to(device)
-            y_channel = y_channel
-            #print("shape di image_y_noised")
-            #print(image_y_noised.shape)
-            y_channel_list.append(y_channel)
-            total+=1
-
-            images_y_batch = torch.stack(y_channel_list).to(device)
-            #print("shape di batch")
-            #print(images_y_batch.shape)
-
-        
-        
-    
-        #print("j")
-        #print(j)
-        detected_fingerprints = RevealNet(images_y_batch)
-        detected_fingerprints = (detected_fingerprints > 0).long()
-        #print("shape firme")
-        #print(detected_fingerprints.shape)
-        
-        for l in enumerate(detected_fingerprints):
             
-            #print(f"firma{j}")
-            #print(detected_fingerprints[l])
-            #to calculate the accuracy in retrieving the fingerprint (eventually perturbated)
-            bitwise_accuracy += (detected_fingerprints[l].detach() == fingerprint).float().mean().sum().item()
-            #print(bitwise_accuracy)
-            j = j + 1
-            
-            
-        
-            
-    #print(img_noise_path)
-    psnr = main(image_directory, img_noise_path)
+    psnr = mainp(image_directory, img_noise_path)
     psnr_array.append(psnr)
     std_array.append(std)
-    std +=10
-    bitwise_accuracy = bitwise_accuracy/j
+    #bitwise_accuracy = bitwise_accuracy/j
+    bitwise_accuracy = maina(img_noise_path, decoder_path)
     accuracy_array.append(bitwise_accuracy)
+    std +=10
     
 
 print(std_array)
@@ -209,4 +138,3 @@ print(accuracy_array)
 print(psnr_array)
 
 plotting(std_array,accuracy_array,psnr_array,"Noise std","Bitwise accuracy","PSNR (dB)","Gaussian noise")
-
